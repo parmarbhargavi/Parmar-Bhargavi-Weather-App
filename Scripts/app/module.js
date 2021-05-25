5e07f086804bef453257dd4edd75e7831c68dd19
parmarbhargavi <64101007+parmarbhargavi@users.noreply.github.com>
2021-05-24 21:12:30 -0400

    Delete Scripts/app directory

--- Scripts/app/module.js
+++
@@ -1,6 +0,0 @@
-﻿var controllerProvider = null;
-var app = angular.module('myWeatherApp', ['ng', 'ngRoute'], [
-    "$controllerProvider", function ($controllerProvider) {
-        controllerProvider = $controllerProvider;
-    }
-]);
